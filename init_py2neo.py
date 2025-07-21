a1a81aa06b64c48d3f853cc4c09d322ea79b733a
kkkkk <sikaixu0325@gmail.com>
2025-07-21 22:06:06 +0800

    Add files via upload

---
+++ init_py2neo.py
@@ -0,0 +1,45 @@
+# _*_ coding:utf-8 _*_
+"""
+@File     : init_py2neo.py
+@Project  : 你倒是敲代码啊
+@Time     : 2024/3/16 17:32
+@Author   : KUN
+@Software : PyCharm
+@License  : (C)Copyright 2018-2028, Taogroup-NLPR-CASIA
+@Last Modify Time      @Version     @Desciption
+--------------------       --------        -----------
+2024/3/16 17:32        1.0             None
+"""
+
+from py2neo import Graph, Node
+import pandas as pd
+
+
+def init_py2neo():
+    t1 = pd.read_excel('rel2.xlsx')
+    g = Graph(
+        "http://127.0.0.1:7474", auth=("neo4j", "12345678"),name="neo4j")
+    g.delete_all()
+    is_create = []
+    for index, i in t1[['sub_type', 'sub_name']].iterrows():
+        if i['sub_name'] not in is_create:
+            is_create.append(i['sub_name'])
+            node = Node(i['sub_type'], name=i['sub_name'])
+            g.create(node)
+    for index, i in t1[['obj_type', 'obj_name']].iterrows():
+        if i['obj_name'] not in is_create:
+            is_create.append(i['obj_name'])
+            node = Node(i['obj_type'], name=i['obj_name'])
+            g.create(node)
+
+    for index, i in t1.iterrows():
+        query = "match(p:%s),(q:%s) where p.name='%s'and q.name='%s' create (p)-[rel:%s{name:'%s'}]->(q)" % (
+            i['sub_type'], i['obj_type'], i['sub_name'], i['obj_name'], i['rel_type'], i['rel_name'])
+        print(query)
+        try:
+            g.run(query)
+        except Exception as e:
+            print(e)
+
+
+init_py2neo()
---
+++ 地图3.py
@@ -0,0 +1,15 @@
+# _*_ coding:utf-8 _*_
+"""
+@File     : 地图3.py
+@Project  : dj3
+@Time     : 2024/5/4 16:45
+@Author   : KUN
+@Software : PyCharm
+@License  : (C)Copyright 2018-2028, Taogroup-NLPR-CASIA
+@Last Modify Time      @Version     @Desciption
+--------------------       --------        -----------
+2024/5/4 16:45        1.0             None
+"""
+
+if __name__ == '__main__':
+    pass
